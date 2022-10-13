b7475b437172607b087ad90f041a3258a4dbd1d3
Mijail Q Mariano <mijail.mariano@gmail.com>
2022-10-13 11:40:47 -0400

    131100OCT22 updates

--- exercises.sql
+++ exercises.sql
@@ -103,4 +103,29 @@ SELECT name
     FROM Employee
         ORDER BY name;
 
+-----
+-- DATE: Thursday, October 13th 2022
 
+-- Query_010 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
+-- Output one of the following statements for each record in the table:
+
+-- Equilateral: It's a triangle with  sides of equal length.
+-- Isosceles: It's a triangle with sides of equal length.
+-- Scalene: It's a triangle with  sides of differing lengths.
+-- Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+-- using a "CASE" function/method
+-- SELECT CASE 
+--     WHEN (A + B <= C) OR (A + C <= B) OR (B + C <= A) THEN "Not A Triangle"
+--     WHEN (A = B) AND (B = C) THEN "Equilateral",
+--     WHEN (A = B) OR (A = C) OR (B = C) THEN "Isosceles"
+--     ELSE "Scalene"
+-- END tuple
+-- FROM TRIANGLES;
+
+-- using "IF" "ELSE" statements
+SELECT IF(A + B <= C OR B + C <= A OR A + C <= B, "Not A Triangle",
+        IF(A = B AND B = C, "Equilateral",
+        IF(A = B OR B = C OR A = C, "Isosceles", "Scalene")))
+FROM TRIANGLES;
+        
